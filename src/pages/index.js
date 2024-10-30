c40b8112445bd41d527bcc8983b8d6a442005f20
zerOsand <mmaillet@hamilton.edu>
2024-10-30 16:29:07 -0400

    popup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import { contentArea, searchBarStyle, buttonStyle } from '../utils/styles'
 import Searchbar from '../components/Searchbar'
+import CreatePopup from '../components/Popup'
 
-const handleCreateClick = () => {
-    /* TODO 13 */
-    console.log("Create!");
-};
+// const handleCreateClick = () => {
+//     /* TODO 13 */
+//     console.log("Create!");
+// };
 
 const Home = () => {
+
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleCreateClick = () => {
+        setShowPopup(true); // Show the popup when the button is clicked
+    };
+
+    const handleClosePopup = () => {
+        setShowPopup(false); // Hide the popup when the close button is clicked
+    };
+
     return (
 	    <div style={contentArea}>
-	    <div style={searchBarStyle}>
-	    <Searchbar />
-	    <button onClick={handleCreateClick} style={buttonStyle}>Create</button>
-	    </div>
-	    {/* TODO 16 */}
-	    <h2>Text here
-	</h2>
-	    <p>More text here
-	</p>
+			<div style={searchBarStyle}>
+				<Searchbar />
+				<button onClick={handleCreateClick} style={buttonStyle}>Create</button>
+			</div>
+			{/* TODO 16 */}
+			<h2>Text here</h2>
+			<p>More text here</p>
+
+			{showPopup && <CreatePopup onClose={handleClosePopup} />}
 	    </div>
     )
 }
