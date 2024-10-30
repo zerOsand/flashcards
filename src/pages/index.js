0b5e6b50037e6ddaa3b28a4c3d960b89952aca7d
bd <bdunaisky@umass.edu>
2024-10-30 18:45:36 -0400

    Clicklist component allowing item tracking, initial flashcard list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,55 @@
+import { useEffect, useState } from 'react'
 import { contentArea, searchBarStyle, buttonStyle } from '../utils/styles'
 import Searchbar from '../components/Searchbar'
+import ClickList from '../components/ClickList'
 
-const handleCreateClick = () => {
-    /* TODO 13 */
-    console.log("Create!");
+const Flashcard = (flashcard, active) => {
+    /* TODO */
+    return (
+            <div>
+            <div>{flashcard.value}</div>
+            {active === true ? console.log(flashcard.text) : null}
+        </div>
+    )
 };
 
+/* TODO */
+const flashcards = [
+    { value: 'beren', text: 'empty-handed', tags: ['human', 'outlaw', 'mortal']},
+    { value: 'thingol', text: 'king of doriath', tags: ['elf']},
+    { value: 'luthien', text: 'princess of doriath', tags: ['elf']},
+    { value: 'huan', text: 'hound of valinor', tags: ['dog']},
+];
+
 const Home = () => {
+    const [activeIndex, setActiveIndex] = useState(null)
+
+    const handleCreateClick = () => {
+        /* TODO 13 */
+        console.log("Create!");
+    };
+
+    const handleCardClick = (index) => {
+        setActiveIndex(index)
+    }
+
     return (
-	    <div style={contentArea}>
-	    <div style={searchBarStyle}>
-	    <Searchbar />
-	    <button onClick={handleCreateClick} style={buttonStyle}>Create</button>
-	    </div>
-	    {/* TODO 16 */}
-	    <h2>Text here
-	</h2>
-	    <p>More text here
-	</p>
-	    </div>
+        <div style={contentArea}>
+        <div style={searchBarStyle}>
+          <Searchbar />
+          <button onClick={handleCreateClick} style={buttonStyle}>Create
+          </button>
+        </div>
+          <div className="container">
+        <div className="left">
+          <ClickList active={activeIndex} list={flashcards} item={Flashcard} event={handleCardClick} />
+        </div>
+        <div className="right">
+          <h2>This displays a close-up view of a single flashcard.
+          </h2>
+        </div>
+          </div>
+      </div>
     )
 }
 
--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,25 +1,34 @@
 export const pageStyle = {
-    padding: '1vw',
+	padding: '1vw',
 }
 
 export const contentArea = {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '20px',
-    border: '2px solid #333',
-    minHeight: '100vh',
-    overflowY: 'auto',
+	display: 'flex',
+	flexDirection: 'column',
+	margin: '20px',
+	border: '2px solid #333',
+	minHeight: '100vh',
+	overflowY: 'auto',
 }
 
 export const searchBarStyle = {
-    display: 'flex',
-    borderColor: '#333',
+	display: 'flex',
+	borderColor: '#333',
 };
 
 export const buttonStyle = {
-    borderRadius: '0',
-    borderColor: '#333',
-    color: '#000',
-    backgroundColor: '#33cc33',
-    font: 'bold 10pt Arial, Helvetica, sans-serif',
+	borderRadius: '0',
+	borderColor: '#333',
+	color: '#000',
+	backgroundColor: '#33cc33',
+	font: 'bold 10pt Arial, Helvetica, sans-serif',
 };
+
+export const cardPreviewStyle = {
+	borderStyle: 'solid',
+	borderRadius: '1',
+	borderColor: '#000',
+	padding: '8vw',
+	margin: '4vw',
+	textAlign: 'center'
+}
